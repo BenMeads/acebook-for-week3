e56c7d91dc75349ccf66d06375d35bfb33b92999
ben <ben.meads@makers.tech>
2025-02-12 19:05:46 +0000

    fixed appspec.yml mongo db install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -14,4 +14,19 @@ else
   mkdir ${DIR}
 fi
 
+# Install MongoDB on yum-based systems (Amazon Linux, CentOS, RHEL)
+
+# Create a MongoDB repository file
+echo "[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo
+
+# Install MongoDB
 sudo yum install -y mongodb-org
+
+# Start and enable the MongoDB service
+sudo systemctl start mongod
+sudo systemctl enable mongod
