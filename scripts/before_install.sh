bc9f66d2b3a8fea85c4261eec8bc1aef25a0b71b
ben <ben.meads@makers.tech>
2025-02-12 19:34:44 +0000

    fixed appspec.yml mongo db installto community 5

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -16,15 +16,15 @@ fi
 
 # Install MongoDB on yum-based systems (Amazon Linux, CentOS, RHEL)
 
-# Create a MongoDB repository file
-echo "[mongodb-org-6.0]
+# Create a MongoDB repository file for version 5.0
+echo "[mongodb-org-5.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/5.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-5.0.repo
 
-# Install MongoDB
+# Install MongoDB Community Edition 5.0
 sudo yum install -y mongodb-org
 
 # Start and enable the MongoDB service
