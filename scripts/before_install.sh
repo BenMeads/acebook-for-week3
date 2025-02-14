3b3a0e74ebf2e121717813765b98548f765ec0ab
ben <ben.meads@makers.tech>
2025-02-14 10:58:19 +0000

    updated to not install mongodb

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -17,15 +17,16 @@ fi
 # Install MongoDB on yum-based systems (Amazon Linux, CentOS, RHEL)
 
 # Create a MongoDB repository file for version 5.0
-echo "[mongodb-org-5.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/5.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-5.0.repo
+# Mongo 5 does not work
+# echo "[mongodb-org-5.0]
+# name=MongoDB Repository
+# baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/5.0/x86_64/
+# gpgcheck=1
+# enabled=1
+#gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-5.0.repo
 
 # Install MongoDB Community Edition 5.0
-sudo yum install -y mongodb-org
+#sudo yum install -y mongodb-org
 
 # Start and enable the MongoDB service
 sudo systemctl start mongod
