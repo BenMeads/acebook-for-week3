1fe4075e320d51072f7f808684e39b1f79337b97
ben <ben.meads@makers.tech>
2025-02-15 16:10:31 +0000

    updated to comment out some of to stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-sudo pkill node
+# the below caused the piepline to fail as "operation not permitted"
+# pkill node
