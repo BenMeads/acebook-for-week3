de8b7e8a194792b788599efac48052580918796b
ben <ben.meads@makers.tech>
2025-02-15 15:58:31 +0000

    updated to install mongodb8 corrected file name of mongo db repo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -24,7 +24,7 @@ name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://pgp.mongodb.com/server-8.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-5.0.repo
+gpgkey=https://pgp.mongodb.com/server-8.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo
 
 # Install MongoDB Community Edition 5.0
 sudo yum install -y mongodb-org
